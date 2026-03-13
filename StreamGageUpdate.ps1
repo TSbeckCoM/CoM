8312bc18204b1ab126ccffaee87935b7de7fd347
TSbeckCoM <timothy.m.schierenbeck@co.maui.hi.us>
2026-03-13 00:45:53 -1000

    Update StreamGageUpdate.ps1

--- StreamGageUpdate.ps1
+++ StreamGageUpdate.ps1
@@ -92,18 +92,12 @@ foreach ($r in $results | Sort-Object SiteCode) {
         $r.SiteCode, $value, $r.SiteName, $minor, $major, $r.Timestamp) -ForegroundColor $color
 }
 
-# Path to your synced SharePoint folder
-# C:\Users\tmschi\OneDrive - County of Maui\Documents
+# Write latest.json cleanly
 $outputPath = Join-Path -Path $PWD -ChildPath "latest.json"
-Set-Content -Path $outputPath -Value $json
-
-# Convert your results to JSON and write to file
 $results | ConvertTo-Json -Depth 10 | Out-File -FilePath $outputPath -Encoding utf8
 
-Write-Output "Working directory: $PWD"
 Write-Host "Dashboard JSON written to GitHub repo."
 
-$json = Get-Content latest.json -Raw
 
 
 
