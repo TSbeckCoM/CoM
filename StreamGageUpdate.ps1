1d05a552f5fb59b0046b3e2e5d7cd685695f3edb
TSbeckCoM <timothy.m.schierenbeck@co.maui.hi.us>
2026-04-06 16:55:54 -1000

    Update StreamGageUpdate.ps1

--- StreamGageUpdate.ps1
+++ StreamGageUpdate.ps1
@@ -1,41 +1,4 @@
-# ---------------------------------------------
-# Flood stage lookup table (Minor & Major only)
-# ---------------------------------------------
-$floodStages = @{
-    "16415600" = @{ Minor = 10;   Major = $null }
-    "16400000" = @{ Minor = 9.5;  Major = 19.9 }
-    "16638500" = @{ Minor = 6;    Major = 8.61 }
-    "16620000" = @{ Minor = 6;    Major = 7.9 }
-    "16618000" = @{ Minor = 6;    Major = 12.4 }
-    "16614000" = @{ Minor = 6;    Major = 11 }
-    "16604500" = @{ Minor = 8.5;  Major = $null }
-    "16605500" = @{ Minor = $null; Major = $null }
-    "16587000" = @{ Minor = 3.5;  Major = 7.3 }
-    "16552800" = @{ Minor = 5.4;  Major = 8.2 }
-    "16518000" = @{ Minor = 10;   Major = 13.5 }
-    "16508000" = @{ Minor = 7.5;  Major = 12.1 }
-    "16501200" = @{ Minor = 7;    Major = 12.4 }
-}
-
-
-# ---------------------------------------------
-# Kona max lookup table (per station, by SiteCode)
-# ---------------------------------------------
-$konaMax = @{
-    "16415600" = @{ Kona1 = $null; Kona2 = $null }
-    "16400000" = @{ Kona1 = $null; Kona2 = $null }
-    "16638500" = @{ Kona1 = $null; Kona2 = $null }
-    "16620000" = @{ Kona1 = $null;  Kona2 = $null }
-    "16618000" = @{ Kona1 = $null;  Kona2 = $null }
-    "16614000" = @{ Kona1 = $null; Kona2 = $null }
-    "16604500" = @{ Kona1 = $null;  Kona2 = $null }
-    "16605500" = @{ Kona1 = 13.4; Kona2 = 13.5 } 
-    "16587000" = @{ Kona1 = $null;  Kona2 = $null }
-    "16552800" = @{ Kona1 = $null;  Kona2 = $null }
-    "16518000" = @{ Kona1 = $null; Kona2 = $null }
-    "16508000" = @{ Kona1 = $null;  Kona2 = $null }
-    "16501200" = @{ Kona1 = $null;  Kona2 = $null }
-}
+. "$PSScriptRoot/thresholds.ps1"
 
 # ---------------------------------------------
 # Pull USGS stream gage data
